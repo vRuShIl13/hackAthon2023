6d11d555b87f9846345194d5211f9df25c02aa63
mespn <manuelen2002@gmail.com>
2023-02-25 13:00:42 -0600

    category css edits

--- club-connect/src/components/category.js
+++ club-connect/src/components/category.js
@@ -17,9 +17,10 @@ export const Category = () => {
             <ul>
               <li>
                 <Event
+                  image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
                   post="Category 1"
                   description="This is going to be the best event ever!"
-                  link= "umanitoba.ca"
+                  link= "https://devclub.ca/"
                 />
               </li>
 
