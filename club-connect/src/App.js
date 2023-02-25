c07bd8e052247eb632ba63e15e15c99c28350d08
Vrushil  Patel <66082759+vRuShIl13@users.noreply.github.com>
2023-02-25 13:05:02 -0600

    css
    minor changes in css

--- club-connect/src/App.js
+++ club-connect/src/App.js
@@ -9,12 +9,12 @@ function App() {
       <header className="App-header">Club-Connect</header>
       <div >
       <Event className = "devHack"
-        post="event 1"
+        post="Hackathon"
         description="This is going to be the best event ever!"
         link= "https://devclub.ca/"
         image = "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
       />
-      
+
   
       </div>
     </div>
