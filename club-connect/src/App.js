ad97f795e3918028b71490be6164e9e34f465573
Vrushil  Patel <66082759+vRuShIl13@users.noreply.github.com>
2023-02-25 11:12:04 -0600

    Added the link option in the event post

--- club-connect/src/App.js
+++ club-connect/src/App.js
@@ -10,14 +10,18 @@ function App() {
       <Event
         post="event 1"
         description="This is going to be the best event ever!"
+        link= "umanitoba.ca"
       />
       <Event
         post="event 2"
         description="This is going to be the best event ever!"
+        link= "google.com"
+
       />
       <Event
         post="event 3"
         description="This is going to be the best event ever!"
+        link= "https://framer.com/projects/Untitled--8Pm1x2UVirWdLBtKdaJs-eKBUt?reason=web-signup&id=325517ef-e665-45e5-b5c7-4f0c853a08ca&preview=1&node=WQLkyLRf1"
       />
       </div>
     </div>
--- club-connect/src/components/event.js
+++ club-connect/src/components/event.js
@@ -3,12 +3,18 @@ import React from 'react'
 
 
 
-const event = ({ post, description }) => {
+const event = ({ image, link ,post, description }) => {
   return (
     <div className="event-post">
         <h3>{post}</h3>
         <p>{description}</p>
-        <button>Find out more </button>
+        <div class = 'more-info-button'>
+            <a href= {link} >
+                <button>More Info</button>
+            </a>
+    
+        </div>
+        
       </div>
   )
 }
