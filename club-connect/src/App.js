73ece84fba74343f159fba5972f19aa41794085e
Om Sevak <omsevak252002@gmail.com>
2023-02-25 17:33:10 -0600

    Added nav bar and carousel

--- club-connect/src/App.js
+++ club-connect/src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Carousel from './components/carousel';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    
+    <div>
+      <NavBar />
+      <Carousel />
+
+    </div>
+
   );
 }
 
