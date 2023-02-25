048f26a3c6241935d29bd2ec37129218d836a5cc
mespn <manuelen2002@gmail.com>
2023-02-25 15:10:51 -0600

    delete test categories

--- club-connect/src/components/category.js
+++ club-connect/src/components/category.js
@@ -26,70 +26,7 @@ export const Category = () => {
                 </div>
               </li>
 
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
-
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
-
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
-
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
-
-              <li>
-              <div className = "card">
-                  <Event
-                    image= "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY"
-                    post="Category 1"
-                    description="This is going to be the best event ever!"
-                    link= "https://devclub.ca/"
-                  />
-                </div>
-              </li>
+              
 
             </ul>
         </div>
