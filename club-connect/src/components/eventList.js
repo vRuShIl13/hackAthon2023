78c89c2da18063c1df6828d7430a502180b57852
Vrushil  Patel <66082759+vRuShIl13@users.noreply.github.com>
2023-02-25 17:28:35 -0600

    vv

--- club-connect/src/components/eventList.js
+++ club-connect/src/components/eventList.js
@@ -1,12 +1,4 @@
-import React, {useState} from 'react'
-import Event from './event'
-import NewCategoryForm from './newCategoryForm'
-import "./category.css"
-//import eventContent from "./eventList"
-
-export const Category = () => {
-
-  const eventContent = [
+const eventContent = [
     {
         image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
         post:"Category 1",
@@ -32,36 +24,8 @@ export const Category = () => {
         link: "https://devclub.ca/"
     }
 
-  ]
+    
 
-  const [categories, setCategories] = useState([]);
+];
 
-  const handleAddCategories = (newCategory) => {
-    setCategories([...categories, newCategory]);
-  }
-
-  return (
-    <div className="category">
-        <h2 id = "category-header">
-            Categories
-        </h2>
-
-        <div className = "category-grid">
-            <ul>
-              <li>
-                <div>
-                  eventContent.
-                </div>
-              </li>
-            </ul>
-        </div>
-
-        <div>
-          <NewCategoryForm onAddItem = {handleAddCategories}/>
-        </div>
-        
-    </div>
-  )
-}
-
-export default Category;
+export default eventContent;
