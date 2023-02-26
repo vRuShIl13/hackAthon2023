b643fb1a7444f971e4d8f0635cba07d4b9d869ad
mespn <manuelen2002@gmail.com>
2023-02-25 21:06:35 -0600

    change link

--- club-connect/src/components/eventList.js
+++ club-connect/src/components/eventList.js
@@ -3,7 +3,7 @@ const EventContent = [
         image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
         post: "Hackathon",
         description: "Learn how to code!",
-        link: "https://devclub.ca/" 
+        link: "https://devclub.ca/hackathon" 
     },
 
     {
