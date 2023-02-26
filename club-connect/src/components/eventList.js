d3ac557ab50b188b1b09dd409a002f5197b2ddf9
Vrushil  Patel <66082759+vRuShIl13@users.noreply.github.com>
2023-02-25 19:25:50 -0600

    images changes

--- club-connect/src/components/eventList.js
+++ club-connect/src/components/eventList.js
@@ -1,34 +1,34 @@
 const EventContent = [
     {
         image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
-        post: "Category 1",
-        description: "This is going to be the best event ever!",
+        post: "Hackathon",
+        description: "Learn how to code!",
         link: "https://devclub.ca/" 
     },
 
     {
-        image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
-        post: "Category 1",
-        description: "This is going to be the best event ever!",
-        link: "https://devclub.ca/" 
+        image: require("../assets/phy.png"),
+        post: "Physics Study Time",
+        description: "Its now or never!",
+        link: "https://docs.google.com/forms/d/e/1FAIpQLSepoNEaihiPLopIrgX15Ivo8yMkfYF4KSomY3TG83ehAeCXvA/viewform" 
     },
     {
-        image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
-        post: "Category 1",
-        description: "This is going to be the best event ever!",
-        link: "https://devclub.ca/" 
+        image: require("../assets/samosa.png"),
+        post: "Food Sale!",
+        description: "Taste the delicious!",
+        link: "" 
     },
     {
-        image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
-        post: "Category 1",
-        description: "This is going to be the best event ever!",
-        link: "https://devclub.ca/" 
+        image: require("../assets/conqherEve.png"),
+        post: "The Conqher Event",
+        description: "For the Asper Women!",
+        link: "https://www.eventbrite.ca/e/conqher-event-tickets-526820643727?fbclid=PAAaYIEEMrUkbxROJxOZb0eyEnngICqM2EeIgGP71MFba9fIYnqc90j9NYDgc" 
     },
     {
-        image: "https://media.licdn.com/dms/image/C5622AQEp1OwuO8WezA/feedshare-shrink_1280/0/1676658904610?e=1680134400&v=beta&t=2WKQ2dCJs-HacAtIgMBUNwiAcD0gYO0oM9JkdHioSoY",
-        post: "Category 1",
-        description: "This is going to be the best event ever!",
-        link: "https://devclub.ca/" 
+        image: require("../assets/meetingMartis.png"),
+        post: "Lunch With Eternity Martis",
+        description: "Learn from Experienced!",
+        link: "" 
     }
 ]
 
