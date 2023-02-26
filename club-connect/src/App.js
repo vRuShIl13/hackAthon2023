3e3148a23f0562844ed753a51c6b82a13fffb614
mespn <manuelen2002@gmail.com>
2023-02-25 18:28:15 -0600

    Full merge

--- club-connect/src/App.js
+++ club-connect/src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Carousel from './components/carousel';
 import NavBar from './components/NavBar';
+import Category from './components/category';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Carousel />
+
+      <Category/>
 
     </div>
 
